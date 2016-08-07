fe08090909c5863a7698054c3f2be2165d68760e
Phillip J. Wolfram <phillipwolfram@gmail.com>
2016-08-07 00:50:23 -0600

    @loader_path option for OSX

--- cgal/build.sh
+++ cgal/build.sh
@@ -5,6 +5,13 @@
 # with just the path
 # had been using SYS_PREFIX, but PREFIX is probably more appropriate since
 # having gmp as a requirement will install it to PREFIX, not SYS_PREFIX
-cmake -DGMP_LIBRARIES="-L$PREFIX/lib -lgmp" -DGMP_INCLUDE_DIR=$PREFIX/include -DCMAKE_INSTALL_PREFIX=$PREFIX '-DCMAKE_INSTALL_RPATH=$ORIGIN/../lib' .
-make 
+if [ "$(uname)" == "Linux" ]; then
+  cmake -DGMP_LIBRARIES="-L$PREFIX/lib -lgmp" -DGMP_INCLUDE_DIR=$PREFIX/include -DCMAKE_INSTALL_PREFIX=$PREFIX '-DCMAKE_INSTALL_RPATH=$ORIGIN/../lib' .
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  cmake -DGMP_LIBRARIES="-L$PREFIX/lib -lgmp" -DGMP_INCLUDE_DIR=$PREFIX/include -DCMAKE_INSTALL_PREFIX=$PREFIX '-DCMAKE_INSTALL_RPATH=@loader_path/../lib' .
+fi
+
+make
 make install
--- python-cgal-bindings/build.sh
+++ python-cgal-bindings/build.sh
@@ -3,7 +3,12 @@
 # ugly hack - not sure how to get around this
 SWIG_VER=`swig -swiglib | sed -e 's|^.*/share/swig/||'`
 export SWIG_LIB=$PREFIX/share/swig/${SWIG_VER}
-cmake -DBUILD_JAVA=OFF -DSWIG_DIR=$SYS_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX .
+if [ "$(uname)" == "Linux" ]; then
+  cmake -DBUILD_JAVA=OFF -DSWIG_DIR=$SYS_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX '-DCMAKE_INSTALL_RPATH=$ORIGIN/../lib' .
+fi
+if [ "$(uname)" == "Darwin" ]; then
+  cmake -DBUILD_JAVA=OFF -DSWIG_DIR=$SYS_PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX '-DCMAKE_INSTALL_RPATH=@loader_path/../lib' .
+fi
 # on wakari, don't use -j b/c of RAM constraints
 make -j2
 cp -av build-python/CGAL $SP_DIR/CGAL
